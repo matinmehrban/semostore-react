15352f213159397e8cbe782f8352c9827be485d8
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 01:21:38 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>به فروشگاه SEMO خوش آمدید</h1>
+      <p>فروش انواع لوازم دیجیتال با کیفیت بالا</p>
+    </div>
+  );
+}
