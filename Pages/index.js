bbd898f40282f221889883e719677ad0f75bb476
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 14:15:11 +0330

    Delete Pages directory

--- Pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>به فروشگاه SEMO خوش آمدید 🎉</h1>
-      <p>محصولات جدید به زودی بارگذاری خواهند شد.</p>
-    </div>
-  );
-}
