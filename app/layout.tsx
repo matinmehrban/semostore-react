cf99eb6b9956c45c089bdf1f467dc4e02d49cf21
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 19:31:50 +0330

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+// app/layout.tsx
+import './globals.css';
+import { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="fa">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
