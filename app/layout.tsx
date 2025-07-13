b59b3fac7daaab7fb6ca82affdd5b91713a07cdd
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 21:53:19 +0330

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// app/layout.tsx
+import './globals.css';
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'SemoStore - فروشگاه گجت و دیجیتال',
+  description: 'جدیدترین محصولات دیجیتال را در SemoStore ببینید.',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="fa">
+      <body>{children}</body>
+    </html>
+  );
+}
