e317bbb9e9d834831946a81e0f3d83f3c6a05135
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 11:38:51 +0330

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>به فروشگاه SEMO خوش آمدید</h1>
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>به فروشگاه SEMO خوش آمدید 🎉</h1>
+      <p>محصولات جدید به زودی بارگذاری خواهند شد.</p>
     </div>
-  )
+  );
 }
