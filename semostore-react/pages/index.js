8165e5958db9d7469215b065958ae63291cc8734
matinmehrban <matiinmehrban@gmail.com>
2025-07-13 14:26:42 +0330

    Add files via upload

---
+++ semostore-react/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-8">
+      <h1 className="text-4xl font-bold mb-4">به فروشگاه SEMO خوش آمدید</h1>
+      <p className="text-lg text-gray-400 mb-10">فروش لوازم دیجیتال با بهترین کیفیت</p>
+    </main>
+  )
+}
